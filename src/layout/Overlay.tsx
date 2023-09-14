a2fea241ca56f38d660f33f2f8ff1908da0dedd6
Paul <72733450+paul1995tu@users.noreply.github.com>
2023-09-14 17:55:39 +0200

    fix import overlay

--- src/layout/Overlay.tsx
+++ src/layout/Overlay.tsx
@@ -1,7 +1,7 @@
 import type {ParentComponent } from "solid-js";
 import { children, createEffect,  } from "solid-js";
 
-import {createUniqueClassName} from "../helper/helper.ts";
+import {createUniqueClassName} from "../helper/helper";
 
 const trackZindexBackground: {[key:string]:string} = {};
 const trackZindexForeground: {[key:string]:string} = {};

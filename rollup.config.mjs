793f11374e73fa5c2024190d57309b71e0117912
Paul <72733450+paul1995tu@users.noreply.github.com>
2023-10-02 02:14:15 +0200

    rework

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,12 @@
 import withSolid from "rollup-preset-solid";
 
 import {vanillaExtractPlugin} from '@vanilla-extract/rollup-plugin';
+import css from "rollup-plugin-import-css";
 
 export default withSolid({
     input: "./src/index.tsx",
     targets: ["esm", "cjs"],
-    plugins: [vanillaExtractPlugin()],
+    plugins: [vanillaExtractPlugin(), css()],
     output: {
         dir: "dist",
         preserveModules: true,
